749073459e7ef63f8a8237249b694341ae7d5f04
Mark deVilliers <markdevilliers@gmail.com>
2013-11-29 13:06:06 +0000

    Solution to Problem 14 : Duplicate the elements of a list.

---
+++ ninetynine/014_fourteen.fsx
@@ -0,0 +1,20 @@
+/// Problem 14 : Duplicate the elements of a list.
+/// /// Example in F#: 
+/// 
+/// > dupli [1; 2; 3]
+/// [1;1;2;2;3;3]
+
+
+let dupli ls =
+    ls |> List.map (fun x -> [x;x;])
+       |> List.concat
+
+let result = dupli [1; 2; 3]
+printfn "%A" result 
+
+let dupli' ls =
+    ls |> List.collect (fun x -> [x;x;])
+
+
+let result' = dupli' [1; 2; 3]
+printfn "%A" result'
